3cf84503aa74e13e2f90772e6b0fc82a81d977ee
Omar Sanseviero <osanseviero@gmail.com>
2022-03-10 16:26:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ def hello(inp):
 
 # For information on Interfaces, head to https://gradio.app/docs/
 # For user guides, head to https://gradio.app/guides/
+# For Spaces usage, head to https://huggingface.co/docs/hub/spaces
 iface = gradio.Interface(
   fn=hello,
   inputs='text',
   outputs='text',
   title='Hello World', 
-  description='The simplest Hosted interface.')  
+  description='The simplest interface!')  
 
 iface.launch()
