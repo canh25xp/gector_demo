7d8bcd80af0682ba6f4c9c916950b1a59e53c28b
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-05 14:04:03 +0700

    disable flagging

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import gradio
 
+
 def hello(inp):
-  return f"Hello {inp}!!"
+    return f"Hello {inp}!!"
+
 
 # For information on Interfaces, head to https://gradio.app/docs/
 # For user guides, head to https://gradio.app/guides/
 # For Spaces usage, head to https://huggingface.co/docs/hub/spaces
 iface = gradio.Interface(
-  fn=hello,
-  inputs='text',
-  outputs='text',
-  title='Hello World', 
-  description='The simplest interface!')  
+    fn=hello,
+    inputs="text",
+    outputs="text",
+    title="Hello World",
+    description="The simplest interface!",
+    allow_flagging="never"
+)
 
 iface.launch()
